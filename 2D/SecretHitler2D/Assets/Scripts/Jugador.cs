6fda09f8ef0c1b5effacd92d3fe6de14285a6f3c
serbolsd <32022302+serbolsd@users.noreply.github.com>
2020-05-20 12:46:05 -0600

    Se agrego una clase para cada jugador, esta clase cuenta con su id de jugador, su,rol,, su cargo, y su voto. int Rol = -1;//0=liberal 1=Facistas 2=Hitler int Cargo = -1;//0=Nada 1=Presidente 2=Viseprecidente int siONo = -1;//0=si 1=no -1=espera

---
+++ 2D/SecretHitler2D/Assets/Scripts/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    int ID = -1;
+    int Rol = -1;//0=liberal 1=Facistas 2=Hitler
+    int Cargo = -1;//0=Nada 1=Presidente 2=Viseprecidente
+    int siONo = -1;//0=si 1=no -1=espera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
