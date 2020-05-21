75ffbd121607d61558e24a8fab0d2fe465b8d9b6
serbolsd <32022302+serbolsd@users.noreply.github.com>
2020-05-20 20:54:21 -0600

    creacion de baraja con 17 cartas, 11 facistas y 6 liberales

---
+++ 2D/SecretHitler2D/Assets/Scripts/BarajaDePolizas.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarajaDePolizas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[]baraja;
+    public GameObject cartaPreFab;
+    GameObject []desechadas;
+    GameObject []colocadas;
+    GameObject []disponibles;
+
+    const int numCartas = 17;
+    const int numCartasFas = 11;
+    const int numCartasLib = 6;
+    void Start()
+    {
+        onStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onStart()
+    {
+        baraja = new GameObject[numCartas];
+        for (int i = 0; i < numCartas; i++)
+        {
+            
+            baraja[i] = Instantiate(cartaPreFab,this.transform);
+            if (i<numCartasFas)
+            {
+                baraja[i].GetComponent<CartaDePoliza>().tipoDeCarta = 1;
+            }
+            else
+            {
+                baraja[i].GetComponent<CartaDePoliza>().tipoDeCarta = 0;
+            }
+        }
+    }
+
+    void barajear()
+    {
+
+    }
+}
---
+++ 2D/SecretHitler2D/Assets/Scripts/CartaDePoliza.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartaDePoliza : MonoBehaviour
+{
+    public int tipoDeCarta = 1; //0=liberal 1=facista
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
