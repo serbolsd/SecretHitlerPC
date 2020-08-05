77222b493ad1b7b4430233d33ffb5612fe4bb1fd
LAPTOP-BGAV0320\ramse <idv18c.rguerrero@uartesdigitales.edu.mx>
2020-08-05 15:58:11 -0500

    //Se agregó el diseño y funcionamiento básico de los menús //Se intentó agregar sonido para los botones, se necesita arreglar

---
+++ 2D/SecretHitler2D/Assets/Scripts/codeBtnSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class codeBtnSound : MonoBehaviour
+{
+    public AudioSource g_audioSource { get { return GetComponent<AudioSource>(); } }
+    public Button g_btn { get { return GetComponent<Button>(); } }
+    public AudioClip g_audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+
+        g_btn.onClick.AddListener(PlaySound);
+    }
+
+    void PlaySound()
+    {
+        g_audioSource.PlayOneShot(g_audioClip);
+    }
+}
